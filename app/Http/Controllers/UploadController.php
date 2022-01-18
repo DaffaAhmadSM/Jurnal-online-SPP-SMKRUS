27b80e0b5e10e8ba75d2e67468eca1387efc9530
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2022-01-18 09:44:55 +0700

    [DA][feat] import excel

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -4,15 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Imports\InvoiceImport;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Facades\Excel;
 
 class UploadController extends Controller
 {
+    
         public function import(){
             return view('import');
         }
         public function store(Request $request) 
     {
+        $user = backpack_user()->id;
+        $_SESSION['user'] = $user;
 
         $file = $request->file;
 
