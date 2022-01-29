bc97879f65b6e69119260230cd2136c35a160cd3
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2022-01-29 11:09:30 +0700

    [DA][feat] editing response json

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Invoice;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -17,14 +18,21 @@ class LoginController extends Controller
     public function store(Request $request)
     {
         $fields = $request->validate([
-            'nisn' => 'required|string',
+            'nis' => 'required|string',
         ]);
-        //check nisn
-        $user = Invoice::where('nisn', $fields['nisn'])->first();
+        $namaSiswa = DB::table('invoices')
+                    ->select('nama')
+                    ->where('nis', $fields['nis'])
+                    ->first();
+        //check nis
+        $user = Invoice::where('nis', $fields['nis'])->first();
         $token = $user->createToken('token')->plainTextToken; 
 
         return [
-            'user' => $fields['nisn'],
+            'user' => $fields['nis'],
+            'nama_siswa' => $namaSiswa->nama,
+            'wali_kelas' => $user->wali_kelas->name,
+            'kelas' => $user->Wali_kelas->kelas,    
             'token' => $token,
         ];
         
